441f8e8bae1108d2f719479a159fae2c699afdc7
Stephen Carlin <scarlinj@gmail.com>
2023-07-31 22:05:37 -0700

    added About, Gallery, Nav, and PhotoList components.  Connected to index.js to nivagate.

--- src/components/Nav/index.js
+++ src/components/Nav/index.js
@@ -4,7 +4,18 @@ import { capitalizeFirstLetter } from "../../utils/helpers";
 
 function Nav(props) {
   const {
-    categories = [],
+    categories = [{
+      name: "commercial",
+      description:
+        "Photos of grocery stores, food trucks, and other commercial projects",
+    },
+    { name: "portraits", description: "Portraits of people in my life" },
+    { name: "food", description: "Delicious delicacies" },
+    {
+      name: "landscape",
+      description: "Fields, farmhouses, waterfalls, and the beauty of nature",
+    },
+  ],
     setCurrentCategory,
     currentCategory,
   } = props;
@@ -17,7 +28,9 @@ function Nav(props) {
 //     return item;
 //   };
 
-  
+function categorySelected(name) {
+  console.log(`${name} clicked`)
+}
 
   return (
     <header className="flex-row px-1">
@@ -38,20 +51,28 @@ function Nav(props) {
               Contact
             </span>
           </li>
+          {/* use .map function to have more DRY code - define catergories in an array above the return statement, listing each catregory as an object */}
+          {/* When mapping over anything in JSX, outermost element must have key attribute set to be something unique. This helps React keep track of items in the virtual DOM */}
           {categories.map((category) => (
             <li
               className={`mx-1 ${
                 currentCategory.name === category.name
                 }`}
+                // used category.name as a key to find unique categories, since categories will not share name
+                // Oftentimes, key will be an id
               key={category.name}
             >
-              <span
+              {/* provide function when user clicks category */}
+              <span onClick={categorySelected} >
+                {category.name}
+              </span>
+              {/* <span
                 onClick={() => {
                   setCurrentCategory(category);
                 }}
               >
                 {capitalizeFirstLetter(category.name)}
-              </span>
+              </span> */}
             </li>
           ))}
         </ul>
