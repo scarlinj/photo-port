9b05e1b2ebaf88fea8a049e1cd5e66a8180183d5
Stephen Carlin <scarlinj@gmail.com>
2023-11-30 11:46:40 -0800

    hardcoded image for food in Gallery index.js.  Displays correctly in gallery.  Need to edit gallery to display images for each section.

--- src/components/Gallery/index.js
+++ src/components/Gallery/index.js
@@ -1,14 +1,25 @@
 import React from 'react';
 import PhotoList from '../PhotoList';
+import photo from '../../assets/small/food/0.jpg';
 import { capitalizeFirstLetter } from '../../utils/helpers';
 
 function Gallery(props) {
-  const { currentCategory } = props;
+  // const { currentCategory } = props;
+  const currentCategory = {
+      name: "commercial",
+      description:
+        "Photos of grocery stores, food trucks, and other commercial projects",
+    };
   return (
     <section>
       <h1 data-testid="h1tag">{capitalizeFirstLetter(currentCategory.name)}</h1>
       <p>{currentCategory.description}</p>
-      <PhotoList category={currentCategory.name} />
+      <div>
+        <img
+          src={photo}
+          />
+      </div>
+      {/* <PhotoList category={currentCategory.name} /> */}
     </section>
   );
 }
