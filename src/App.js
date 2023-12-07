b80856e573ec8879a466e14f88710a737f64c7ba
Stephen Carlin <scarlinj@gmail.com>
2023-12-06 20:56:29 -0800

    fixed import for Nav in App.js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import Nav from "./components/Nav/Nav";
+import Nav from "./components/Nav";
 import About from './components/About';
 import Gallery from './components/Gallery';
 import Contact from './components/Contact';
